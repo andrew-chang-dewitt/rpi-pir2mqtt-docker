cd7a422bea36611b4b891343f74f711fce406fd4
Andrew DeWitt <adwtt90@gmail.com>
2020-01-21 16:52:14 -0500

    refactored utilities & updated control flow logic for motion() to allow future expansion for 2 sensors

--- app/sense.py
+++ app/sense.py
@@ -17,7 +17,7 @@ from mqtt import MqttHelper
 #
 # setup GPIO pin
 #
-PIR_PIN = 7
+PIR_PIN = configs.A_PIN
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
 
@@ -30,27 +30,31 @@ mqtt = MqttHelper(configs).connect()
 #
 # With MQTT connection established, handle PIR sensor
 #
-SENSOR_ID = os.getenv('SENSOR_ID', 'test_sensor')
-topic = "security/motion_sensors/" + SENSOR_ID
-def motion(PIR_PIN):
-    log("motion detected, sending mqtt event to {topic}".format(topic=topic))
+def motion(pin):
+    if pin == configs.A_PIN:
+        sensor_id = configs.A_NAME
+    elif pin == configs.B_PIN:
+        sensor_id = configs.B_NAME
+
+    topic = "security/motion_sensors/" + sensor_id
+    utils.log("motion detected, sending mqtt event to {topic}".format(topic=topic))
     res = {
-        'timestamp': timestamp(),
-        'message': "motion detected at {SENSOR_ID}".format(SENSOR_ID=SENSOR_ID),
+        'timestamp': utils.timestamp(),
+        'message': "motion detected at {sensor_id}".format(sensor_id=sensor_id),
         'motion': True
     }
 
-    mqttc.publish(topic, json.dumps(res))
+    mqtt.publish(topic, json.dumps(res))
 
 try:
-    log("Adding GPIO listener on {PIR_PIN}".format(PIR_PIN=PIR_PIN))
+    utils.log("Adding GPIO listener on {PIR_PIN}".format(PIR_PIN=PIR_PIN))
     GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motion)
-    log("Waiting for motion detection")
+    utils.log("Waiting for motion detection")
     while 1:
         time.sleep(3600)
 except KeyboardInterrupt:
-    log("\nQuitting motion detection...")
+    utils.log("\nQuitting motion detection...")
     GPIO.cleanup()
-    log("GPIO event detection stopped & cleaned")
+    utils.log("GPIO event detection stopped & cleaned")
 
     mqtt.disconnect()

28efc817263d94f2c368229396b4e8ca07dcf474
Andrew Chang-DeWitt <adwtt90@gmail.com>
2020-01-21 12:05:36 -0500

    partial commit, MQTT functionality mostly pulled out

---
+++ app/configs.py
@@ -0,0 +1,9 @@
+MQTT_HOST = "192.168.0.139"
+MQTT_PORT = 1883
+
+A_PIN = 7
+A_NAME = "living room"
+
+B_PIN = None
+B_NAME = None
+
---
+++ app/mqtt.py
@@ -0,0 +1,56 @@
+import time
+import socket
+import paho.mqtt.client as mqtt
+
+import utils
+
+class MqttHelper:
+    def __init__(self, configs):
+        mqtt.Client.connected_flag = False # creates connection flag for looping
+        self._client = mqtt.Client(socket.gethostname())
+        self._client.on_connect = self._on_connect # binding client connection callback to above fn
+        self._MQTT_HOST = configs.MQTT_HOST
+        self._MQTT_PORT = configs.MQTT_PORT
+
+    def connect(self):
+        self._client.loop_start()
+        utils.log(
+            "Attempting to establish connection to MQTT Host @ "
+            "{MQTT_HOST}:{MQTT_PORT}"
+            .format(
+                MQTT_HOST=self._MQTT_HOST,
+                MQTT_PORT=self._MQTT_PORT))
+
+        self._client.connect(self._MQTT_HOST, self._MQTT_PORT)
+        while not self._client.connected_flag:
+            utils.log("Waiting for connection")
+            time.sleep(1)
+
+        return self._client
+
+    def disconnect(self):
+        self._client.loop_stop()
+        self._client.disconnect()
+        self._client.connected_flag = False
+        log("MQTT client disonnected")
+
+        return self._client
+
+    def _on_connect(self, client, userdata, flags, rc):
+        if rc==0:
+            client.connected_flag=True
+            utils.log(
+                "Connection established to MQTT Host @ "
+                "{MQTT_HOST}:{MQTT_PORT}"
+                .format(
+                    MQTT_HOST=self._MQTT_HOST,
+                    MQTT_PORT=self._MQTT_PORT))
+        else:
+            utils.log(
+                "Connection failed to MQTT Host @ "
+                "{MQTT_HOST}:{MQTT_PORT}"
+                .format(
+                    MQTT_HOST=self._MQTT_HOST,
+                    MQTT_PORT=self._MQTT_PORT))
+
+            utils.log("Failure reason code: {rc}".format(rc=rc))

6407261bba6987a891d8aeaba4addb73c9ec8d37
Andrew Chang-DeWitt <adwtt90@gmail.com>
2020-01-20 22:03:48 -0500

    timestamps

--- sense.py
+++ sense.py
@@ -5,8 +5,18 @@
 # 
 import os
 import time
+from datetime import datetime
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
+
+# 
+# basic logger
+#
+def timestamp():
+  return datetime.now().isoformat()
+
+def log(msg):
+  print(timestamp() + ": " + msg)
 
 # 
 # setup GPIO pin
@@ -30,19 +40,19 @@ mqttc = mqtt.Client("living room motion")
 def on_connect(client, userdata, flags, rc):
   if rc==0:
     client.connected_flag=True
-    print("Connection established to MQTT Host @ {MQTT_HOST}:{MQTT_PORT}".format(MQTT_HOST=MQTT_HOST, MQTT_PORT=MQTT_PORT))
+    log("Connection established to MQTT Host @ {MQTT_HOST}:{MQTT_PORT}".format(MQTT_HOST=MQTT_HOST, MQTT_PORT=MQTT_PORT))
   else: 
-    print("Connection failed to MQTT Host @ {MQTT_HOST}:{MQTT_PORT}".format(MQTT_HOST=MQTT_HOST, MQTT_PORT=MQTT_PORT))
-    print("Failure reason code: {rc}".format(rc=rc))
+    log("Connection failed to MQTT Host @ {MQTT_HOST}:{MQTT_PORT}".format(MQTT_HOST=MQTT_HOST, MQTT_PORT=MQTT_PORT))
+    log("Failure reason code: {rc}".format(rc=rc))
 
 mqttc.on_connect = on_connect # binding client connection callback to above fn
 
 mqttc.loop_start()
-print("Attempting to establish connection to MQTT Host @ {MQTT_HOST}:{MQTT_PORT}".format(MQTT_HOST=MQTT_HOST, MQTT_PORT=MQTT_PORT))
+log("Attempting to establish connection to MQTT Host @ {MQTT_HOST}:{MQTT_PORT}".format(MQTT_HOST=MQTT_HOST, MQTT_PORT=MQTT_PORT))
 mqttc.connect(MQTT_HOST, MQTT_PORT)
 
 while not mqttc.connected_flag:
-  print("Waiting for connection")
+  log("Waiting for connection")
   time.sleep(1)
 
 #
@@ -51,22 +61,22 @@ while not mqttc.connected_flag:
 SENSOR_ID = os.getenv('SENSOR_ID', 'test_sensor')
 topic = "security/motion_sensors/" + SENSOR_ID
 def motion(PIR_PIN):
-  print("motion detected, sending mqtt event to {topic}".format(topic=topic))
-  mqttc.publish(topic, "detected")
+  log("motion detected, sending mqtt event to {topic}".format(topic=topic))
+  mqttc.publish(topic, timestamp() + ": motion detected")
 
 try:
-  print("Adding GPIO listener on {PIR_PIN}".format(PIR_PIN=PIR_PIN))
+  log("Adding GPIO listener on {PIR_PIN}".format(PIR_PIN=PIR_PIN))
   GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motion)
-  print("Waiting for motion detection")
+  log("Waiting for motion detection")
   while 1:
     time.sleep(3600)
 #
 # Use KeyboardInterrupt as quit signal to cleanup GPIO & MQTT loop
 except KeyboardInterrupt:
-  print("\nQuitting motion detection...")
+  log("\nQuitting motion detection...")
   GPIO.cleanup()
-  print("GPIO event detection stopped & cleaned")
+  log("GPIO event detection stopped & cleaned")
 
   mqttc.loop_stop()
   mqttc.disconnect()
-  print("MQTT client disonnected")
+  log("MQTT client disonnected")

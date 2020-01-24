e88c075557d91201270f197f58ed02d93ba9439c
Andrew Chang-DeWitt <adwtt90@gmail.com>
2020-01-24 06:49:24 -0500

    still not able to gracefully exit

--- app/sense.py
+++ app/sense.py
@@ -13,102 +13,109 @@ import utils
 from mqtt import MqttHelper
 from gpio import GpioHelper
 
-exit = Event()
-#
-# setup GPIO pins
-#
-gpio = GpioHelper(configs.SENSOR_A, configs.SENSOR_B)
+class App:
+    def __init__(self):
+        self.exit = Event() #
+        # setup GPIO pins
+        #
+        self.gpio = GpioHelper(configs.SENSOR_A, configs.SENSOR_B)
 
-#
-# setup mqtt client, then
-# initialize mqtt connection & begin loop
-#
-mqtt = MqttHelper(configs).connect()
+        #
+        # setup mqtt client, then
+        # initialize mqtt connection & begin loop
+        #
+        self.mqtt = MqttHelper(configs).connect()
+        self.fault_signal("FAILED")
 
-#
-# With MQTT connection established, handle PIR sensor
-#
-def motion(pin_returned):
-    sensor_id = gpio.PINS[pin_returned]
-    topic = configs.TOPIC + sensor_id
-    motion = "MOTION" if gpio.is_rising(pin_returned) else "CLEAR"
 
-    utils.log(
-        "{motion} on pin {pin_returned}, "
-        "sending mqtt event to {topic}"
-        .format(
-            motion=motion,
-            pin_returned=pin_returned,
-            topic=topic
+    def motion(self, pin_returned):
+        sensor_id = self.gpio.PINS[pin_returned]
+        topic = configs.TOPIC + sensor_id
+        state = "MOTION" if self.gpio.is_rising(pin_returned) else "CLEAR"
+
+        utils.log(
+            "{state} on pin {pin_returned}, "
+            "sending mqtt event to {topic}"
+            .format(
+                state=state,
+                pin_returned=pin_returned,
+                topic=topic
+            )
         )
-    )
 
-    res = {
-        'id' : sensor_id,
-        'motion': motion,
-        'timestamp': utils.timestamp(),
-    }
+        res = {
+            'id' : sensor_id,
+            'state': state,
+            'timestamp': utils.timestamp(),
+        }
 
-    mqtt.publish(topic, json.dumps(res), retain=True)
+        self.mqtt.publish(topic, json.dumps(res), retain=True)
 
-def fault_signal(fault_state):
-    if fault_state == "FAILED" or fault_state == "OK":
-        state = fault_state
-    else:
-        raise ValueError("'{fault_state}' is not a valid input for `fault_signal()`")
+    def fault_signal(self, fault_state):
+        if fault_state == "FAILED" or fault_state == "OK":
+            state = fault_state
+        else:
+            raise ValueError("'{fault_state}' is not a valid input for `fault_signal()`")
 
-    topic = configs.TOPIC + "fault"
-    res = {
-        'id': 'fault',
-        'state': state,
-        'timestamp': utils.timestamp(),
-    }
+        topic = configs.TOPIC + "fault"
+        res = {
+            'id': 'fault',
+            'state': state,
+            'timestamp': utils.timestamp(),
+        }
 
-    utils.log(
-        "fault state set to {state}"
-        "sending mqtt event to {topic}"
-        .format(
-            state=state,
-            topic=topic,
+        utils.log(
+            "fault state set to {state}"
+            "sending mqtt event to {topic}"
+            .format(
+                state=state,
+                topic=topic,
+            )
         )
-    )
 
-    mqtt.publish(topic, json.dumps(res), retain=True)
+        self.mqtt.publish(topic, json.dumps(res), retain=True)
 
-fault_signal("FAILED")
+    def main(self):
+        def cb(pin_returned):
+            return self.motion(pin_returned)
 
-def main():
-    # setup inside loop checking for
-    # end signals
-    while not exit.is_set():
-        gpio.start(motion)
-        fault_signal("OK")
-        exit.wait(6000)
+        while not self.exit.is_set():
+            self.gpio.start(cb)
+            self.fault_signal("OK")
+            self.exit.wait()
 
-    # cleanup
-    fault_signal("FAILED")
-    gpio.stop()
-    mqtt.disconnect()
+        # cleanup
+        self.fault_signal("FAILED")
+        self.gpio.stop()
+        self.mqtt.disconnect()
 
-def quit(signo, _frame):
-    utils.log(
-        "Interrupted by {signo}, shutting down"
-        .format(signo=signo)
-    )
-    exit.set()
+    def quit(self):
+        def cb(signo, _frame):
+            utils.log(
+                "Interrupted by {signo}, shutting down"
+                .format(signo=signo)
+            )
+            self.exit.set()
+
+        return cb
 
 utils.log("{n} is __name__".format(n=__name__))
 
 if __name__ == "__main__":
     import signal
+
+    app = App()
+
     utils.log("Registering signal handlers")
 
-    for sig in ('TERM', 'HUP', 'INT'):
-        signal.signal(
-            getattr(
-                signal, 'SIG'+sig),
-            quit
-        )
+    signal.signal(
+        signal.SIGINT,
+        app.quit()
+    )
+    signal.signal(
+        signal.SIGTERM,
+        app.quit()
+    )
 
-        utils.log("Starting app")
-    main()
+    utils.log("Starting app")
+    app.main()

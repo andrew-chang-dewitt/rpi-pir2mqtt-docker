6c0efe8a2250dace2ddf4059bbf49d67b284e45b
Andrew DeWitt <adwtt90@gmail.com>
2020-02-01 03:40:38 -0500

    updating w/ better inline docs and pylint compliance

--- src/sense.py
+++ src/sense.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Entry script for the rpi-security-gpio2mqtt
+application. covers actually running the app
+as well as very rudimentary error handling
+"""
 
 #
 # import dependencies
@@ -13,28 +18,62 @@ from mqtt import MqttHelper
 from gpio import GpioHelper
 
 class App:
-    def __init__(self, error_handler):
+    """
+    The Application itself is housed here.
+    Inits and stores helper classes as well as
+    the app data as instance variables
+    """
+
+    def __init__(self, err_handler):
+        """
+        Application constructor, accepts an
+        error handler function to stored as
+        an instance variable available
+        throughout the running application.
+
+        Also inits helper classes and begins
+        setting them up.
+
+        Lastly, inits an instance variable for
+        signaling an exit request as False.
+        Changing this to True tells the app
+        to exit.
+        """
         # initilize running variable for tracking quit state
         self.exit = False
 
-        self.error_handler = error_handler
+        self.error_handler = err_handler
 
         # load configuration
         self.config = Configs.load('/src/configuration.yaml')
 
         # setup GPIO pins
-        self.gpio = GpioHelper(self.config.sensors)
+        self.gpio = GpioHelper(self.config.sensor_list)
 
         # setup mqtt client, then
         # initialize mqtt connection & begin loop
         self.mqtt = MqttHelper(
-                self.config.mqtt_host,
-                self.config.mqtt_port).connect()
+            self.config.mqtt_host,
+            self.config.mqtt_port).connect()
 
         self.fault_signal("FAILED")
 
     def event_detected(self, pin_returned):
-        sensor = self.config.sensors[pin_returned]
+        """
+        An instance method passed to a callback
+        for RPi.GPIO's event detection method.
+        Setting it up as an instance method
+        passed to the callback allows access to
+        `self` while keeping the method in
+        which the callback is needed
+        uncluttered.
+
+        The core purpose of this method is to
+        send MQTT messages signaling sensor
+        state on GPIO events.
+        """
+
+        sensor = self.config.sensor_list[pin_returned]
         topic = self.config.root_topic + sensor.topic
         state = sensor.determine_state(self.gpio.is_rising(pin_returned))
 
@@ -57,7 +96,16 @@ class App:
         self.mqtt.publish(topic, json.dumps(res), retain=True)
 
     def fault_signal(self, fault_state):
-        if fault_state == "FAILED" or fault_state == "OK":
+        """
+        Used to set and publish the cureent app
+        status. If sending "OK", the app is
+        running as expected, but if a "FAILED"
+        is sent, then the app is broadcasting
+        that it is currently unable to process
+        the sensors correctly and is unreliable
+        """
+
+        if fault_state in ("FAILED", "OK"):
             state = fault_state
         else:
             raise ValueError("'{fault_state}' is not a valid input for `fault_signal()`")
@@ -81,15 +129,31 @@ class App:
         self.mqtt.publish(topic, json.dumps(res), retain=True)
 
     def run(self):
-        def cb(pin_returned):
-            # wrap callback in a try/except that rethrows errors to the main thread
-            # so that the app doesn't keep chugging along thinking everything is okay
+        """
+        Used to run the application.
+
+        Defines a callback that delegates to
+        the `event_detected` method above for
+        multi-threaded handling of GPIO events,
+        then initiates GPIO event listeners
+        with said callback and starts a loop to
+        wait for events.
+        """
+        def callback(pin_returned):
+            """
+            wrap callback in a try/except that
+            rethrows errors to the main thread
+            so that the app doesn't keep
+            chugging along thinking everything
+            is okay
+            """
             try:
                 return self.event_detected(pin_returned)
-            except Exception as e:
-                self.error_handler(e, self.quit)
+            # pylint: disable=broad-except
+            except Exception as err:
+                self.error_handler(err, self.quit)
 
-        self.gpio.start_listening(cb)
+        self.gpio.start_listening(callback)
 
         while not self.exit:
             self.fault_signal("OK")
@@ -97,6 +161,12 @@ class App:
 
 
     def quit(self):
+        """
+        Kills the loop started in `run()` by
+        setting the `exit` variable to True,
+        Then signals a system failure and
+        performs system cleanup.
+        """
         self.exit = True
 
         # cleanup
@@ -105,12 +175,21 @@ class App:
         self.mqtt.disconnect()
         utils.log("rpi-pir2mqtt successfully shut down")
 
-def error_handler(exception, cb):
+def error_handler(exception, callback):
+    """
+    an overly simplified error handler. used as
+    a global method to allow access before an
+    `App' instance is even initiated.`
+    """
     utils.log("An unexpected error has occurred, exiting app...")
-    cb()
+    callback()
     raise exception
 
-def sig_handler(signo, _frame):
+def sig_handler(_signo, _frame):
+    """
+    A callback to referr modified signal
+    handlers to `App.quit()`
+    """
     utils.log("sig_handler processing quit signal")
     APP.quit()
 
@@ -124,5 +203,5 @@ try:
 except SystemExit:
     utils.log("SystemExit caught, quitting...")
     APP.quit()
-except Exception as e:
-    error_handler(e, APP.quit())
+except Exception as err: # pylint: disable=broad-except
+    error_handler(err, APP.quit())

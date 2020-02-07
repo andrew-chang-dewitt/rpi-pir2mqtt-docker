40cd5d432824f7abcdd27a55b305d236d0c6d3eb
Andrew Chang-DeWitt <adwtt90@gmail.com>
2020-02-07 13:27:19 -0500

    pep257 compliance on utils

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,18 @@
+"""Module exporting shared functions.
+
+Functions:
+    timestamp -- For getting the current time in an ISO string.
+    log       -- For logging to stdout
+"""
+
 from datetime import datetime
 
 
 def timestamp():
+    """Return a string representing the current date/time in ISO format."""
     return datetime.now().isoformat()
 
 
 def log(msg):
+    """Wrap the print builtin adding a timestamp at the beginning."""
     print(timestamp() + ": " + msg)

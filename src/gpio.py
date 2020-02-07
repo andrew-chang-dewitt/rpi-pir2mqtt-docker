ffd3f29e1c3b324e69ce6828d6be8e351ffe2df2
Andrew Chang-DeWitt <adwtt90@gmail.com>
2020-02-07 13:20:33 -0500

    events, gpio, & mqtt all passing pep257

--- src/gpio.py
+++ src/gpio.py
@@ -1,45 +1,61 @@
-import json
-import time
-import RPi.GPIO as GPIO
+"""A wrapper on RPi.GPIO; handles setting up, reading, & tearing down pins.
+
+Classes:
+    GpioHelper -- A simplified wrapper on RPi.GPIO.
+"""
+from typing import Callable
+import RPi.GPIO as io
 
 from src import utils
-from src import sensors
 
 
 class GpioHelper:
-    def __init__(self, sensors_list):
-        self.input = GPIO.input
+    """Simplified wrapper on RPi.GPIO. Handles setup & simplifies connections.
 
+    Methods:
+        start_listening -- Start listening for HIGH or LOW on all pins.
+        stop_listening  -- Wrapper on the RPi.GPIO.cleanup method.
+    """
+
+    def __init__(self, sensors_list: dict):
+        """Init a GpioHelper instance with the given sensors."""
         if not sensors_list:
             raise ValueError("At least one sensor must be given")
 
-        self.PINS = []
-        GPIO.setmode(GPIO.BCM)
+        self.__pins = []
+        io.setmode(io.BCM)
 
         for (pin, sensor) in sensors_list.items():
-            self.PINS.append(pin)
+            self.__pins.append(pin)
 
             if sensor.pull_up or sensor.pull_down:
                 if sensor.pull_up:
-                    pull_up_down = GPIO.PUD_UP
+                    pull_up_down = io.PUD_UP
                 elif sensor.pull_down:
-                    pull_up_down = GPIO.PUD_DOWN
+                    pull_up_down = io.PUD_DOWN
 
-                GPIO.setup(
+                io.setup(
                     pin,
-                    GPIO.IN,
+                    io.IN,
                     pull_up_down=pull_up_down)
             else:
-                GPIO.setup(pin, GPIO.IN)
+                io.setup(pin, io.IN)
 
-    def start_listening(self, callback):
-        for pin in self.PINS:
+    def start_listening(self, callback: Callable[[int], bool]):
+        """Start listening for HIGH or LOW on all pins.
+
+        Arguments:
+            callback -- a function to be called when a GPIO event is detected.
+        """
+        for pin in self.__pins:
             utils.log("Adding GPIO listener on {pin}".format(pin=pin))
-            GPIO.add_event_detect(pin, GPIO.BOTH, callback=callback)
+            io.add_event_detect(pin, io.BOTH, callback=callback)
 
         utils.log("Waiting for motion detection")
 
-    def stop_listening(self):
+    @staticmethod
+    def stop_listening():
+        """Execute RPi.GPIO.cleanup method."""
         utils.log("Quitting motion detection...")
-        GPIO.cleanup()
+        io.cleanup()
         utils.log("GPIO event detection stopped & cleaned")
